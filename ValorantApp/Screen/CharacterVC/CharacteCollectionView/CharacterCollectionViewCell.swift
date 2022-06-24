1bdca9c3029c4cbc4d6d30720d9ae560bc1e09f9
Furkan Erzurumlu <furkanerzurumlu99@yahoo.com>
2022-06-24 22:26:53 +0300

    Add characterCollectionViewCell

---
+++ ValorantApp/Screen/CharacterVC/CharacteCollectionView/CharacterCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CharacterCollectionViewCell.swift
+//  ValorantApp
+//
+//  Created by Furkan Erzurumlu on 24.06.2022.
+//
+
+import UIKit
+
+class CharacterCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
