7cb41f0de611d94297404b046a3fd88b0a7b5750
Furkan Erzurumlu <furkanerzurumlu99@yahoo.com>
2022-07-01 21:52:08 +0300

    CharacterVM update

--- ValorantApp/Screen/CharacterVC/CharacterVM.swift
+++ ValorantApp/Screen/CharacterVC/CharacterVM.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+protocol CharacterVMProtocol: AnyObject{
+    
+}
+
+protocol CharacterVMDelegate: CharacterVMProtocol{
+    func getUpcomingData()
+}
+
+protocol CharacterVMDelegateOutputs: AnyObject{
+    
+}
+
+enum CharacterVMOutputs {
+    //case succes()
+    case error(String)
+}
+
+class CharacterVM: CharacterVMDelegate {
+    
+    var delegate: CharacterVMDelegateOutputs?
+    var network: charecterNetworking = charecterNetworking()
+    
+    func getUpcomingData() {
+        network.getUpComingCharacter{ [weak self] (response) in
+            guard let response = response, let self = self else {
+                return
+            }
+        }
+    }
+}
