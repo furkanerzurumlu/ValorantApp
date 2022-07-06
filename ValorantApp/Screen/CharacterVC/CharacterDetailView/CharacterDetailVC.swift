3682294d5dfda12726780dcf0bc3d3377584ea38
Furkan Erzurumlu <furkanerzurumlu99@yahoo.com>
2022-07-06 14:49:54 +0300

    Slot update

--- ValorantApp/Screen/CharacterVC/CharacterDetailView/CharacterDetailVC.swift
+++ ValorantApp/Screen/CharacterVC/CharacterDetailView/CharacterDetailVC.swift
@@ -16,19 +16,30 @@ class CharacterDetailVC: UIViewController {
     @IBOutlet weak var characterRole: UILabel!
     @IBOutlet weak var characterRoleDescription: UILabel!
     
-    //Abilities Slot1
     
+    //Abilities Slot1
     @IBOutlet weak var abilitiesDisplayIcon: UIImageView!
     @IBOutlet weak var abilitiesSlot: UILabel!
     @IBOutlet weak var abilitiesDisplayName: UILabel!
     @IBOutlet weak var abilitiesDiscription: UILabel!
     
     //Abilities Slot2
-    
     @IBOutlet weak var abilitiesDisplayIcon2: UIImageView!
     @IBOutlet weak var abilitiesSlot2: UILabel!
     @IBOutlet weak var abilitiesDisplayName2: UILabel!
     @IBOutlet weak var abilitiesDiscription2: UILabel!
+    
+    //Abilities Slot3
+    @IBOutlet weak var abilitiesDisplayIcon3: UIImageView!
+    @IBOutlet weak var abilitiesSlot3: UILabel!
+    @IBOutlet weak var abilitiesDisplayName3: UILabel!
+    @IBOutlet weak var abilitiesDiscription3: UILabel!
+    
+    //Abilities Slot4
+    @IBOutlet weak var abilitiesDisplayIcon4: UIImageView!
+    @IBOutlet weak var abilitiesSlot4: UILabel!
+    @IBOutlet weak var abilitiesDisplayName4: UILabel!
+    @IBOutlet weak var abilitiesDiscription4: UILabel!
     
     
     var viewModel: CharacterDetailVM?
@@ -38,6 +49,7 @@ class CharacterDetailVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
        
+        
         
         characterDetailImageView.downloadedImage(from: data?.fullPortraitV2 ?? "")
         characterDisplayName.text = data?.displayName
@@ -50,14 +62,27 @@ class CharacterDetailVC: UIViewController {
         abilitiesSlot.text = data?.abilities[0].slot.rawValue
         abilitiesDisplayName.text = data?.abilities[0].displayName
         abilitiesDiscription.text = data?.abilities[0].abilityDescription
+    
         
         //Abilities Slot Setup2
         abilitiesDisplayIcon2.downloadedImage(from: data?.abilities[1].displayIcon ?? "")
-        abilitiesSlot.text = data?.abilities[1].slot.rawValue
+        abilitiesSlot2.text = data?.abilities[1].slot.rawValue
         abilitiesDisplayName2.text = data?.abilities[1].displayName
         abilitiesDiscription2.text = data?.abilities[1].abilityDescription
         
-    }
+        //Abilities Slot Setup3
+        abilitiesDisplayIcon3.downloadedImage(from: data?.abilities[2].displayIcon ?? "")
+        abilitiesSlot3.text = data?.abilities[2].slot.rawValue
+        abilitiesDisplayName3.text = data?.abilities[2].displayName
+        abilitiesDiscription3.text = data?.abilities[2].abilityDescription
+        
+        //Abilites Slot Setup4
+        abilitiesDisplayIcon4.downloadedImage(from: data?.abilities[3].displayIcon ?? "")
+        abilitiesSlot4.text = data?.abilities[3].slot.rawValue
+        abilitiesDisplayName4.text = data?.abilities[3].displayName
+        abilitiesDiscription4.text = data?.abilities[3].abilityDescription
+        
+        }
 }
 
 extension UIImageView {
@@ -78,5 +103,14 @@ extension UIImageView {
     func downloadedImage(from link: String, contentMode mode: ContentMode = .scaleAspectFit) {
         guard let url = URL(string: link) else { return }
         downloaded(from: url, contentMode: mode)
+    }
+}
+
+extension UIStackView {
+    func addBackground(color: UIColor) {
+        let subView = UIView(frame: bounds)
+        subView.backgroundColor = color
+        subView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        insertSubview(subView, at: 0)
     }
 }
